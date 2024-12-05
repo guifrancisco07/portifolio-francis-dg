abedd6073c26d2c58283685b1e28b398d4c522ae
guifrancisco07 <151561146+guifrancisco07@users.noreply.github.com>
2024-12-04 21:36:07 -0300

    Add files via upload

---
+++ js/reveal.js
@@ -0,0 +1,284 @@
+window.revelar = ScrollReveal({reset:true})
+
+//TOPO-SITE
+revelar.reveal('.efeito-frase-topo',{
+    duration: 1500,
+    distance: '90px'
+})
+
+revelar.reveal('.efeito-btn-topo',{
+        duration: 1500,
+        distance: '90px',
+        delay: 200
+})
+
+revelar.reveal('.efeito-btn2-topo',{
+    duration: 1500,
+    distance: '90px',
+    delay: 250
+})
+//FIM TOPO
+
+//SECTION ENVIAR FEEDBACK
+revelar.reveal('.efeito-container-feed',{
+    duration: 1500,
+    distance: '90px'
+})
+
+revelar.reveal('.efeito-heading',{
+    duration: 1500,
+    distance: '70px',
+    delay: 100
+})
+
+revelar.reveal('.efeito-estrelas',{
+    duration: 1500,
+    distance: '20px',
+    delay: 200
+})
+
+revelar.reveal('.efeito-textarea',{
+    duration: 1500,
+    distance: '30px',
+    delay: 300
+})
+
+revelar.reveal('.efeito-btn-feed',{
+    duration: 1000,
+    distance: '30px',
+    delay: 350
+})
+//FIM SECTION ENVIAR FEEDBACK
+
+//EFEITO FOOTER
+revelar.reveal('.esconder-container',{
+    duration: 800,
+    distance: '50px',
+    delay: 50
+})
+
+revelar.reveal('.efeito-coluna1-footer',{
+    duration: 800,
+    distance: '50px',
+    delay: 50
+})
+
+revelar.reveal('.efeito-coluna2-footer',{
+    duration: 800,
+    distance: '50px',
+    delay: 150
+})
+
+revelar.reveal('.efeito-coluna3-footer',{
+    duration: 800,
+    distance: '50px',
+    delay: 200
+})
+
+revelar.reveal('.efeito-coluna4-footer',{
+    duration: 800,
+    distance: '50px',
+    delay: 250
+})
+
+revelar.reveal('.efeito-copy-footer',{
+    duration: 800,
+    distance: '50px',
+    delay: 200
+})
+
+revelar.reveal('.efeito-heading-redes-footer',{
+    duration: 800,
+    distance: '50px',
+    delay: 200
+})
+
+revelar.reveal('.efeito-icones-footer',{
+    duration: 800,
+    distance: '50px',
+    delay: 200
+})
+//FIM EFEITO FOOTER
+
+//PADRÃO PARA FORMS
+revelar.reveal('.efeito-container-form',{
+    duration: 1500,
+    distance: '90px'
+})
+
+revelar.reveal('.efeito-txt-img',{
+    duration: 1500,
+    distance: '50px',
+    delay: 100
+})
+
+revelar.reveal('.efeito-heading-form',{
+    duration: 1500,
+    distance: '60px',
+    delay: 200
+})
+
+revelar.reveal('.efeito-input1-form',{
+    duration: 1500,
+    distance: '60px',
+    delay: 300
+})
+
+revelar.reveal('.efeito-input2-form',{
+    duration: 1500,
+    distance: '60px',
+    delay: 400
+})
+
+revelar.reveal('.efeito-input3-form',{
+    duration: 1500,
+    distance: '60px',
+    delay: 500
+})
+
+revelar.reveal('.efeito-input4-form',{
+    duration: 1500,
+    distance: '60px',
+    delay: 600
+})
+
+revelar.reveal('.efeito-criterio-form',{
+    duration: 1500,
+    distance: '60px',
+    delay: 700
+})
+
+revelar.reveal('.efeito-criterio-reset',{
+    duration: 1500,
+    distance: '60px',
+    delay: 350
+})
+
+revelar.reveal('.efeito-status-form',{
+    duration: 1500,
+    distance: '60px',
+    delay: 400
+})
+
+revelar.reveal('.efeito-btn-form',{
+    duration: 1500,
+    distance: '60px',
+    delay: 800
+})
+
+revelar.reveal('.efeito-esquecer-login',{
+    duration: 1500,
+    distance: '60px',
+    delay: 500
+})
+
+revelar.reveal('.efeito-btn1-form',{
+    duration: 1500,
+    distance: '60px',
+    delay: 600
+})
+
+revelar.reveal('.efeito-divisao-form',{
+    duration: 1500,
+    distance: '60px',
+    delay: 650
+})
+
+revelar.reveal('.efeito-btn2-form',{
+    duration: 1500,
+    distance: '60px',
+    delay: 700
+})
+//FIM PADRÃO PARA FORMS
+
+//PÁGINA FEEDBACKS
+revelar.reveal('.efeito-feedback-list',{
+    duration: 1500,
+    distance: '60px',
+    delay: 300
+})
+//FIM PÁGINA FEEDBACKS
+
+//MINHA CONTA
+revelar.reveal('.efeito-container-minha-conta',{
+    duration: 1500,
+    distance: '60px',
+    delay: 200
+})
+//FIM MINHA CONTA
+
+//AGENDAR
+revelar.reveal('.efeito-opcao-servico',{
+    duration: 1500,
+    distance: '60px',
+    delay: 300
+})
+
+revelar.reveal('.efeito-opcao-barbeiro',{
+    duration: 1500,
+    distance: '60px',
+    delay: 400
+})
+
+revelar.reveal('.efeito-opcao-data',{
+    duration: 1500,
+    distance: '60px',
+    delay: 500
+})
+
+revelar.reveal('.efeito-opcao-horario',{
+    duration: 1500,
+    distance: '60px',
+    delay: 550
+})
+//FIM AGENDAR
+
+//MEUS AGENDAMENTOS
+revelar.reveal('.efeito-frase-agendamentos',{
+    duration: 1000,
+    distance: '50px',
+    origin: 'top'
+})
+
+revelar.reveal('.efeito-container-agendamentos',{
+    duration: 1500,
+    distance: '60px',
+    delay: 200
+})
+
+//DELAY DIFERENTE PARA CADA CARD DE AGENDAMENTO
+const agendamentos = document.querySelectorAll('[class^="efeito-card-agendamento-"]');
+
+agendamentos.forEach((element, index) => {
+  const id = element.className.match(/efeito-card-agendamento-(\d+)/)[1]; 
+  const delay = id * 5;
+
+  revelar.reveal(element, {
+    duration: 1000,
+    distance: '50px',
+    delay: delay
+    
+  });
+});
+//FIM MEUS AGENDAMENTOS
+
+//VER SERVICOS
+revelar.reveal('.efeito-container-servicos',{
+    duration: 1500,
+    distance: '60px'
+})
+//DELAY DIFERENTE PARA CADA CARD DE SERVIÇO
+const servicos = document.querySelectorAll('[class^="efeito-card-servico-"]');
+
+servicos.forEach((element, index) => {
+  const id = element.className.match(/efeito-card-servico-(\d+)/)[1]; 
+  const delay = id * 35;
+
+  revelar.reveal(element, {
+    duration: 1000,
+    distance: '50px',
+    delay: delay
+    
+  });
+});
+//FIM VER SERVICOS
